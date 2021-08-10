e6440269c50999c24b35f06e39c0af2a13dc11cc
Mohammad Altamish <mohammadaltamish@gmail.com>
2021-08-10 13:18:21 -0500

    Adding final changes from lesson 1

--- src/main/java/com/altamish/demo/web/HomeController.java
+++ src/main/java/com/altamish/demo/web/HomeController.java
@@ -1,15 +1,32 @@
 package com.altamish.demo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.altamish.demo.domain.User;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("formData", new User());
 		return "index";
 		
 	}
-
+	
+	@RequestMapping(value="/create", method=RequestMethod.POST)
+	public String processFormData(User user, RedirectAttributes attr) {
+		attr.addFlashAttribute("user", user);
+		return "redirect:/display";
+	}
+	
+	@RequestMapping(value="/display", method=RequestMethod.GET)
+	public String displayResult(User user) {
+		
+		return "result";
+	}
 }
